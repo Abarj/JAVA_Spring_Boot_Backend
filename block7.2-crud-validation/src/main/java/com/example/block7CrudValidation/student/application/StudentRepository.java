06ef7aba91fe15beed4f6f45c8926b90528e916e
Álvaro <109460941+Abarj@users.noreply.github.com>
2023-06-29 09:31:51 +0200

    Delete block7.2-crud-validation directory

--- block7.2-crud-validation/src/main/java/com/example/block7CrudValidation/student/application/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.block7CrudValidation.student.application;
-
-import com.example.block7CrudValidation.student.domain.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends JpaRepository<Student, String> {
-}
