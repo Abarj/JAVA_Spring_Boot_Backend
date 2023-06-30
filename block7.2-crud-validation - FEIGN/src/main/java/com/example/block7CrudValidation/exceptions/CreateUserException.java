2239752e3ed76a560c51df949bc018ea7014dc56
Álvaro <109460941+Abarj@users.noreply.github.com>
2023-06-30 13:46:53 +0200

    Delete block7.2-crud-validation - FEIGN directory

--- block7.2-crud-validation - FEIGN/src/main/java/com/example/block7CrudValidation/exceptions/CreateUserException.java
+++
@@ -1,7 +0,0 @@
-package com.example.block7CrudValidation.exceptions;
-
-public class CreateUserException extends Exception{
-    public CreateUserException(String mensajeError) {
-        super(mensajeError);
-    }
-}
