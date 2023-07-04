a208a1ba37782e3811cd3bc57376984efa3ef44a
Álvaro <109460941+Abarj@users.noreply.github.com>
2023-07-04 09:30:39 +0200

    Delete block7.2-crud-validation directory

--- block7.2-crud-validation/src/main/java/com/example/block7crudvalidation/teacher/infrastructure/repository/TeacherRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.block7crudvalidation.teacher.infrastructure.repository;
-
-import com.example.block7crudvalidation.teacher.domain.Teacher;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeacherRepository extends JpaRepository<Teacher, String> {
-}
