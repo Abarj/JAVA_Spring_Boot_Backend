e1cac186b041fb52ebdf541ce902f13d5980a933
alvaro.fbarjau <alvarobarjau@gmail.com>
2023-07-10 13:08:08 +0200

    block10-dockerize-app

---
+++ block10-dockerize-app/src/main/java/com/example/block7crud/Block7CrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.block7crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Block7CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Block7CrudApplication.class, args);
+	}
+
+}
