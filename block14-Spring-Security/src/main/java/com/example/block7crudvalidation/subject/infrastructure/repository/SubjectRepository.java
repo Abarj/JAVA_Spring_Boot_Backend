ea08000a4b7bbc034c4ab48624e6de41245bde90
Álvaro <109460941+Abarj@users.noreply.github.com>
2023-07-21 13:14:12 +0200

    Delete block14-Spring-Security directory

--- block14-Spring-Security/src/main/java/com/example/block7crudvalidation/subject/infrastructure/repository/SubjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.block7crudvalidation.subject.infrastructure.repository;
-
-import com.example.block7crudvalidation.subject.domain.Subject;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SubjectRepository extends JpaRepository<Subject, Integer> {
-}
