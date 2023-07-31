fda62332420d47b660a53430d6930456f8858583
Álvaro <109460941+Abarj@users.noreply.github.com>
2023-07-31 18:06:54 +0200

    Delete block17.2-Spring-Batch-Tasklets directory

--- block17.2-Spring-Batch-Tasklets/src/main/java/com/example/block17/SpringBatchApplication/application/IpersonService.java
+++
@@ -1,9 +0,0 @@
-package com.example.block17.SpringBatchApplication.application;
-
-import com.example.block17.SpringBatchApplication.entity.Person;
-
-import java.util.List;
-
-public interface IpersonService {
-    void saveAll(List<Person> personList);
-}
--- block17.2-Spring-Batch-Tasklets/src/main/java/com/example/block17/SpringBatchApplication/persistence/IPersonDAO.java
+++
@@ -1,7 +0,0 @@
-package com.example.block17.SpringBatchApplication.persistence;
-
-import com.example.block17.SpringBatchApplication.entity.Person;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IPersonDAO extends CrudRepository<Person, Long> {
-}
