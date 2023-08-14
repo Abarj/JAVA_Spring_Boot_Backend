ead9d8be7e157e41fbf0e29e8652d50c30002944
alvaro.fbarjau <alvarobarjau@gmail.com>
2023-08-14 11:42:46 +0200

    block24-Spring-GraalVM-Native-Docker-Image

---
+++ block24-Spring-GraalVM-Native-Docker-Image/src/main/java/com/example/block24SpringGraalVMNativeDockerImage/Block24SpringGraalVmNativeDockerImageApplication.java
@@ -0,0 +1,13 @@
+package com.example.block24SpringGraalVMNativeDockerImage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Block24SpringGraalVmNativeDockerImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Block24SpringGraalVmNativeDockerImageApplication.class, args);
+	}
+
+}
---
+++ block24-Spring-GraalVM-Native-Docker-Image/src/test/java/com/example/block24SpringGraalVMNativeDockerImage/Block24SpringGraalVmNativeDockerImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.block24SpringGraalVMNativeDockerImage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Block24SpringGraalVmNativeDockerImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
