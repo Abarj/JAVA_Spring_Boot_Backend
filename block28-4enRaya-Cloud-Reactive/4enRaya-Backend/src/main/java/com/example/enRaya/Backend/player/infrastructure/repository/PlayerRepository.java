f28ade79c1b122962292c7ed06775fe5086c25f6
Álvaro <109460941+Abarj@users.noreply.github.com>
2023-09-12 21:40:52 +0200

    Delete block28-4enRaya-Cloud-Reactive directory

--- block28-4enRaya-Cloud-Reactive/4enRaya-Backend/src/main/java/com/example/enRaya/Backend/player/infrastructure/repository/PlayerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.enRaya.Backend.player.infrastructure.repository;
-
-import com.example.enRaya.Backend.player.entity.Player;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import reactor.core.publisher.Mono;
-
-public interface PlayerRepository extends ReactiveMongoRepository<Player, String> {
-    Mono<Player> findPlayerByUsername(String username);
-}
--- block28-4enRaya-Cloud-Reactive/4enRaya-Backend/src/main/java/com/example/enRaya/Backend/session/infrastructure/enums/Direction.java
+++
@@ -1,7 +0,0 @@
-package com.example.enRaya.Backend.session.infrastructure.enums;
-
-public enum Direction {
-    HORIZONTAL,
-    VERTICAL,
-    DIAGONAL
-}
