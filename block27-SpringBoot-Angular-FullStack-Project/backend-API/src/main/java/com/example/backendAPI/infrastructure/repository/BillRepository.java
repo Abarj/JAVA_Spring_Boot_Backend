8f57224237af4f16c1c5bf1e9b75c985d57763af
Álvaro <109460941+Abarj@users.noreply.github.com>
2023-09-13 09:33:44 +0200

    Delete block27-SpringBoot-Angular-FullStack-Project directory

--- block27-SpringBoot-Angular-FullStack-Project/backend-API/src/main/java/com/example/backendAPI/infrastructure/repository/BillRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backendAPI.infrastructure.repository;
-
-import com.example.backendAPI.entities.Bill;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BillRepository extends CrudRepository<Bill, Long> {
-}
